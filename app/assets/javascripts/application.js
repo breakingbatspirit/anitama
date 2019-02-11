5fdbe91dce246af2d9241d9bb6a8340b62ca27ff
kazuya000 <shakallabb1ts@icloud.com>
2019-02-11 15:14:29 +0900

    bootstrap aseets folder revise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require turbolinks
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .

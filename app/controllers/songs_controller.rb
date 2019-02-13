1b70ea887698c6ec0bf080a7f8b6771fb6b3c6ff
akqbi168 <akqbi168@gmail.com>
2019-02-13 21:18:35 +0900

    add ボタン押すとフォーム増えるやつ

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,20 @@
 class SongsController < ApplicationController
 
 	def create
+        @song = Song.new(song_params)
+        @song.save
 	end
 
 	def destroy
+        @song = Song.find(params[:id])
+        @song.destroy
 	end
 
 	def update
 	end
+
+    private
+        def song_params
+            params.require(:song).permit(:order, :title, :artist)
+        end
 end

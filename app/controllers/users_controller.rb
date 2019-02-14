e53c61741adbc1c85aa3ff40995a67073c78c776
natsumi-m7 <n.osada0724@gmail.com>
2019-02-14 13:09:25 +0900

    Usersページに検索機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    @search = User.ransack(params{:q})
-    @result = @search.result
+    @search = User.ransack(params[:q])
+    @users = @search.result
   end
 
   def show

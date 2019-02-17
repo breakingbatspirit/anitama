3e6a9948a3e07638cc45933507cddc8c404ebca0
yamadaakiyoshi <ax261.v@gmail.com>
2019-02-17 06:23:21 +0000

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cd.create(album:"コナンコレクション", price:"10000", inventory:"0")
+Cd.create(album:"コナンくーーーーん", price:"20000", inventory:"1")
+Anime.create(anime_title:"コナン")
+Anime.create(anime_title:"コナン")
+
+
+
+
+
+
+# Artist.create(artist_name:"倉木麻衣")
+# Artist.create(artist_name:"愛内里菜")
+# Anime.create(anime_title:"コナン")
+# Anime.create(anime_title:"コナン")
+# Label.create(company:"田中")
+# Label.create(company:"田中")
+# Genre.create(generation:"アニメ")
+# Genre.create(generation:"アニメ")
+# Disc.create()
+# Song.create(title:"Secret of my heart", disc_id:1)
+# Song.create(title:"Time after time", disc_id:2)
+# Song.create(title:"恋はスリル、ショック、サスペンス", disc_id:2)

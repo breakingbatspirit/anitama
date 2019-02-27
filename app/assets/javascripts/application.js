14f3d92662565033aa651898b0d831379a8fa9c7
natsumi-m7 <n.osada0724@gmail.com>
2019-02-27 19:26:33 +0900

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,14 +21,3 @@
 //= require cocoon
 
 
-$(function(){
-  $('.dropdown-menu a').click(function(){
-    //反映先の要素名を取得
-    var visibleTag = $(this).parents('ul').attr('visibleTag');
-    var hiddenTag = $(this).parents('ul').attr('hiddenTag');
-    //選択された内容でボタンの表示を変える
-    $(visibleTag).html($(this).attr('value'));
-    //選択された内容でhidden項目の値を変える
-    $(hiddenTag).val($(this).attr('value'));
-  })
-})
